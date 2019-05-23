73aacbff1e7063380d4b81f0307a71c2ebdffd8f
Ashna Shah-Grover <as19@williams.edu>
2019-05-23 08:15:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(file_path)
   new_hash = {}
   hash.each do |meaning, emoticon|
     new_hash[:get_meaning] ||= {}
-    new_hash[:get_meaning][emoticon[]]
+    new_hash[:get_meaning][emoticon[1]]
     new_hash[:get_emoticon]
 end
 
